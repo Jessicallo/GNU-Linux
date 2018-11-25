d7c8ee6f456c180fcc28837b7fe289fa41dd16c4
Jessicallo <sherlock_vip@163.com>
2018-11-25 22:44:48 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GNU_Linux.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GNU_Linux.dir/main.cpp.o"
+  "GNU_Linux.pdb"
+  "GNU_Linux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GNU_Linux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
